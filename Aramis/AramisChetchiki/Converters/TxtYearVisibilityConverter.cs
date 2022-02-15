41b157c727374c8e79bb8993748f47c9760ca677
Master-MiShutka <trus.mikhail@tut.by>
2022-02-15 17:56:19 +0300

    fixing meter consumption viewer

--- Aramis/AramisChetchiki/Converters/TxtYearVisibilityConverter.cs
+++ Aramis/AramisChetchiki/Converters/TxtYearVisibilityConverter.cs
@@ -7,21 +7,42 @@ namespace TMP.WORK.AramisChetchiki.Converters
 {
     public class TxtYearVisibilityConverter : IMultiValueConverter
     {
+        public double FontSize { get; set; }
+
+        private double txtWidth;
+
+        public TxtYearVisibilityConverter()
+        {
+            try
+            {
+                var formattedText = new System.Windows.Media.FormattedText(
+                    "2222",
+                    CultureInfo.CurrentCulture,
+                    FlowDirection.LeftToRight,
+                    new System.Windows.Media.Typeface("Verdana"),
+                    this.FontSize == 0 ? 14d : this.FontSize,
+                    System.Windows.Media.Brushes.Black,
+                    new System.Windows.Media.NumberSubstitution(),
+                    1);
+
+                this.txtWidth = formattedText.Width;
+            }
+            catch (Exception e)
+            {
+                ;
+            }
+        }
+
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (values != null && values.Length == 3)
+            if (values != null && values.Length == 2)
             {
                 double minValue = values[0] == null ? 0.0 : System.Convert.ToDouble(values[0]);
-                double txtWidth = (values[1] == null || values[1] == DependencyProperty.UnsetValue) ? 0.0 : System.Convert.ToDouble(values[1]);
-                double panelWidth = (values[2] == null || values[2] == DependencyProperty.UnsetValue) ? 0.0 : System.Convert.ToDouble(values[2]);
+                double panelWidth = (values[1] == null || values[1] == DependencyProperty.UnsetValue) ? 0.0 : System.Convert.ToDouble(values[1]);
 
-                if (panelWidth <= minValue * 2)
-                    return Visibility.Collapsed;
-
-                if (double.IsNaN(txtWidth) || txtWidth >= panelWidth)
-                    return Visibility.Collapsed;
-
-                if (txtWidth > 0)
+                if (panelWidth > this.txtWidth)
+                    return Visibility.Visible;
+                else
                     return Visibility.Collapsed;
             }
 
