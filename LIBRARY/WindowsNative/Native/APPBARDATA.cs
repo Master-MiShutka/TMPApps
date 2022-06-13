4f13b0305763943f8909bafa3b78ef7ab68702a5
Master-MiShutka <trus.mikhail@tut.by>
2022-06-13 21:10:11 +0300

    Moved Windows native code code to new library 'WindowsNative'

---
+++ LIBRARY/WindowsNative/Native/APPBARDATA.cs
@@ -0,0 +1,16 @@
+﻿namespace WindowsNative
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct APPBARDATA
+    {
+        public int cbSize;
+        public IntPtr hWnd;
+        public int uCallbackMessage;
+        public int uEdge;
+        public RECT rc;
+        public bool lParam;
+    }
+}
---
+++ LIBRARY/WindowsNative/Native/DwmApi.cs
@@ -0,0 +1,13 @@
+﻿namespace WindowsNative
+{
+    public struct DWMCOLORIZATIONcolors
+    {
+        public uint ColorizationColor;
+        public uint ColorizationAfterglow;
+        public uint ColorizationColorBalance;
+        public uint ColorizationAfterglowBalance;
+        public uint ColorizationBlurBalance;
+        public uint ColorizationGlassReflectionIntensity;
+        public uint ColorizationOpaqueBlend;
+    }
+}
